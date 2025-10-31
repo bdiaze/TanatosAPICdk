b9ffc7ea15ffb237c38d2eaae5055c1aa79b9300
Benjamín <benjamin.diaze@outlook.com>
2025-10-31 16:25:50 -0300

    Se añade lógica de lambda de creación inicial

--- InitialCreationLambda/InitialCreationLambda/Function.cs
+++ InitialCreationLambda/InitialCreationLambda/Function.cs
@@ -1,4 +1,11 @@
 using Amazon.Lambda.Core;
+using Amazon.SecretsManager;
+using InitialCreationLambda.Helpers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Npgsql;
+using System.Diagnostics;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -7,7 +14,26 @@ namespace InitialCreationLambda;
 
 public class Function
 {
-    
+    private readonly IServiceProvider serviceProvider;
+
+    public Function() {
+        var builder = Host.CreateDefaultBuilder();
+        builder.ConfigureServices((context, services) => {
+            #region Singleton AWS Services
+            services.AddSingleton<IAmazonSecretsManager, AmazonSecretsManagerClient>();
+            #endregion
+
+            #region Singleton Helpers
+            services.AddSingleton<SecretManagerHelper>();
+            #endregion
+        });
+
+        var app = builder.Build();
+
+        serviceProvider = app.Services;
+    }
+
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
@@ -16,6 +42,101 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        return input.ToUpper();
+        Stopwatch sw = Stopwatch.StartNew();
+        LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Iniciando proceso de creacion inicial del schema y sus usuarios de aplicacion...");
+
+        string secretArnConnectionString = Environment.GetEnvironmentVariable("SECRET_ARN_CONNECTION_STRING") ?? throw new ArgumentNullException("SECRET_ARN_CONNECTION_STRING");
+        string appName = Environment.GetEnvironmentVariable("APP_NAME") ?? throw new ArgumentNullException("APP_NAME");
+        string appSchemaName = Environment.GetEnvironmentVariable("APP_SCHEMA_NAME") ?? throw new ArgumentNullException("APP_SCHEMA_NAME");
+        string migrationEFBundle = Environment.GetEnvironmentVariable("MIGRATION_EFBUNDLE") ?? throw new ArgumentNullException("MIGRATION_EFBUNDLE");
+
+        if (appSchemaName.Contains('"')) {
+            throw new Exception($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Error con el nombre del schema para app \"{appName}\" - Caracteres invalidos...");
+        }
+
+        LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Obteniendo secreto de conexion a base de datos...");
+
+        SecretManagerHelper secretManagerHelper = serviceProvider.GetRequiredService<SecretManagerHelper>();
+        Dictionary<string, string> connectionStringSecrets = JsonConvert.DeserializeObject<Dictionary<string, string>>(secretManagerHelper.ObtenerSecreto(secretArnConnectionString).Result)!;
+        string connectionString = $"Server={connectionStringSecrets["Host"]};Port={connectionStringSecrets["Port"]};SslMode=prefer;" +
+            $"Database={connectionStringSecrets[$"{appName}Database"]};User Id={connectionStringSecrets[$"{appName}AdmUsername"]}; Password='{connectionStringSecrets[$"{appName}AdmPassword"]}';";
+
+        List<string> retorno = [];
+
+        LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Conectandose a base de datos RDS PostgreSQL [{connectionStringSecrets["Host"]}]...");
+
+        using (NpgsqlConnection conn = new(connectionString)) {
+            conn.Open();
+
+            // Se crea schema...
+            LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Creando el schema para app \"{appName}\" - [Schema: {appSchemaName}]...");
+            try {
+                using NpgsqlCommand cmd = new($"CREATE SCHEMA IF NOT EXISTS \"{appSchemaName}\"", conn);
+                cmd.ExecuteNonQuery();
+            } catch (Exception ex) {
+                string mensaje = $"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Error al crear schema de la app: " + ex;
+                LambdaLogger.Log(mensaje);
+                retorno.Add(mensaje);
+            }
+
+            // Se crea usuario de aplicación...
+            string appUsername = connectionStringSecrets[$"{appName}AppUsername"];
+            if (appUsername.Contains('"')) {
+                throw new Exception($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Error con el nombre de usuario de aplicacion para app \"{appName}\" - Caracteres invalidos...");
+            }
+            string appPassword = connectionStringSecrets[$"{appName}AppPassword"];
+            if (appPassword.Contains('\'')) {
+                throw new Exception($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Error con la contraseña del usuario de aplicacion para app \"{appName}\" - Caracteres invalidos...");
+            }
+
+            LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Creando usuario de aplicacion para app \"{appName}\"...");
+            try {
+                using NpgsqlCommand cmd = new($"CREATE USER \"{appUsername}\" WITH ENCRYPTED PASSWORD '{appPassword}'", conn);
+                cmd.ExecuteNonQuery();
+            } catch (Exception ex) {
+                string mensaje = $"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Error al crear usuario de aplicacion de la app: " + ex;
+                LambdaLogger.Log(mensaje);
+                retorno.Add(mensaje);
+            }
+
+            // Se otorgan permisos sobre el usuario dew aplicación...
+            LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Otorgando permisos para usuario de aplicacion para app \"{appName}\"...");
+            try {
+                // Se habilita que usuario de aplicación pueda usar el nuevo esquema...
+                using NpgsqlCommand cmd = new($"GRANT USAGE ON SCHEMA \"{appSchemaName}\" TO \"{appUsername}\"", conn);
+                cmd.ExecuteNonQuery();
+
+                // Se definen los permisos por defecto para el usuario de aplicación, sobre las tablas creadas por el usuario administrador en el nuevo esquema...
+                using NpgsqlCommand cmd2 = new($"ALTER DEFAULT PRIVILEGES FOR USER \"{connectionStringSecrets[$"{appName}AdmUsername"]}\" IN SCHEMA \"{appSchemaName}\" GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO \"{appUsername}\"", conn);
+                cmd2.ExecuteNonQuery();
+
+                // Se definen los permisos por defecto para el usuario de aplicación, sobre las secuencias creadas por el usuario administrador en el nuevo esquema..
+                using NpgsqlCommand cmd3 = new($"ALTER DEFAULT PRIVILEGES FOR USER \"{connectionStringSecrets[$"{appName}AdmUsername"]}\" IN SCHEMA \"{appSchemaName}\" GRANT USAGE ON SEQUENCES TO \"{appUsername}\"", conn);
+                cmd3.ExecuteNonQuery();
+            } catch (Exception ex) {
+                string mensaje = $"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Error al otorgar permisos para usuario de aplicacion de la app: " + ex;
+                LambdaLogger.Log(mensaje);
+                retorno.Add(mensaje);
+            }
+
+            // Se aplica la migración de EFCore...
+            LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Se inicia migracion EFCore del modelo de datos...");
+            try {
+                string script = File.ReadAllText(migrationEFBundle);
+                using NpgsqlCommand cmd = new(script, conn);
+                cmd.ExecuteNonQuery();
+            } catch (Exception ex) {
+                string mensaje = $"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Error con la migracion EFCore del modelo de datos de la app: " + ex;
+                LambdaLogger.Log(mensaje);
+                retorno.Add(mensaje);
+            }
+
+            conn.Close();
+        }
+
+        LambdaLogger.Log($"[Elapsed Time: {sw.ElapsedMilliseconds} ms] - Ha terminado el proceso de creacion inicial del schema y sus usuarios de aplicacion...");
+
+
+        return JsonConvert.SerializeObject(retorno);
     }
 }
---
+++ InitialCreationLambda/InitialCreationLambda/Helpers/SecretManagerHelper.cs
@@ -0,0 +1,31 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialCreationLambda.Helpers {
+    internal class SecretManagerHelper(IAmazonSecretsManager client) {
+
+        private readonly Dictionary<string, string> secretsValues = [];
+
+        public async Task<string> ObtenerSecreto(string secretArn) {
+            if (!secretsValues.TryGetValue(secretArn, out string? value)) {
+                GetSecretValueResponse response = await client.GetSecretValueAsync(new GetSecretValueRequest {
+                    SecretId = secretArn
+                });
+
+                if (response == null || response.SecretString == null) {
+                    throw new Exception("No se pudo rescatar correctamente el secreto");
+                }
+
+                value = response.SecretString;
+                secretsValues[secretArn] = value;
+            }
+
+            return value;
+        }
+    }
+}
