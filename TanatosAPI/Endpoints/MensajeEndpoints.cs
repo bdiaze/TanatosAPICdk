b008f3085392304f674f850e6aec791ac959eb0a
Benjamín <benjamin.diaze@outlook.com>
2026-02-15 17:57:08 -0300

    Se añade segundo endpoint para ingresar mensajes de forma anónima.

--- TanatosAPI/Endpoints/MensajeEndpoints.cs
+++ TanatosAPI/Endpoints/MensajeEndpoints.cs
@@ -1,4 +1,5 @@
 ﻿using Amazon.Lambda.Core;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Security.Claims;
 using TanatosAPI.Entities.Models;
@@ -11,10 +12,11 @@ namespace TanatosAPI.Endpoints {
 		public static IEndpointRouteBuilder MapMensajeEndpoints(this IEndpointRouteBuilder routes) {
 			RouteGroupBuilder group = routes.MapGroup("/Mensaje");
 			group.MapObtener();
+			group.MapIngresarEndpoint();
 
 			// Endpoints públicos 
 			RouteGroupBuilder publicGroup = routes.MapGroup("/public/Mensaje");
-			publicGroup.MapIngresarEndpoint();
+			publicGroup.MapIngresarAnonimoEndpoint();
 
 			return routes;
 		}
@@ -48,6 +50,8 @@ namespace TanatosAPI.Endpoints {
 				Stopwatch stopwatch = Stopwatch.StartNew();
 
 				try {
+					string sub = user.Identity?.Name ?? throw new Exception("No se incluye la información del usuario.");
+
 					const string ACTION = "contact_form";
 					const float MIN_SCORE = 0.7f;
 
@@ -81,8 +85,14 @@ namespace TanatosAPI.Endpoints {
 					entrada.Correo = entrada.Correo.Trim();
 					entrada.Contenido = entrada.Contenido.Trim();
 
-					// Se obtiene sub si el usuario está autenticado, si no lo está, se deja como null para indicar que es un mensaje anónimo...
-					string? sub = (user.Identity?.IsAuthenticated ?? false) ? user.Identity?.Name : null;
+					// Se valida que el correo sea un correo válido...
+					EmailAddressAttribute emailValidator = new();
+					if (!emailValidator.IsValid(entrada.Correo)) {
+						LambdaLogger.Log(
+							$"[POST] - [Mensaje] - [Ingresar] - [{stopwatch.ElapsedMilliseconds} ms] - [{StatusCodes.Status400BadRequest}] - " +
+							$"El correo ingresado no es válido.");
+						return Results.BadRequest($"El correo ingresado no es válido.");
+					}
 
 					long id = await mensajeDao.Insertar(new Mensaje {
 						Id = 0,
@@ -101,6 +111,79 @@ namespace TanatosAPI.Endpoints {
 				} catch (Exception ex) {
 					LambdaLogger.Log(
 						$"[POST] - [Mensaje] - [Ingresar] - [{stopwatch.ElapsedMilliseconds} ms] - [{StatusCodes.Status500InternalServerError}] - " +
+						$"Ocurrió un error al ingresar el mensaje. " +
+						$"{ex}");
+					return Results.Problem($"Ocurrió un error al procesar su solicitud. {(!environment.IsProduction() ? ex : "")}");
+				}
+			}).RequireAuthorization("Negocios.Write.Self").WithOpenApi();
+
+			return routes;
+		}
+
+		private static IEndpointRouteBuilder MapIngresarAnonimoEndpoint(this IEndpointRouteBuilder routes) {
+			routes.MapPost("/", async (EntMensajeIngresar entrada, IHostEnvironment environment, GoogleRecaptchaHelper googleRecaptchaHelper, MensajeDao mensajeDao) => {
+				Stopwatch stopwatch = Stopwatch.StartNew();
+
+				try {
+					const string ACTION = "contact_form";
+					const float MIN_SCORE = 0.7f;
+
+					(bool valid, string invalidReason, string action, float score) response = await googleRecaptchaHelper.ObtenerAssesment(entrada.RecaptchaToken, ACTION);
+
+					// Se valida que la respuesta sea válida...
+					if (!response.valid) {
+						LambdaLogger.Log(
+							$"[POST] - [Mensaje] - [IngresarAnonimo] - [{stopwatch.ElapsedMilliseconds} ms] - [{StatusCodes.Status400BadRequest}] - " +
+							$"El token de reCAPTCHA no es válido. Razón: {response.invalidReason}.");
+						return Results.BadRequest("El token de reCAPTCHA no es válido.");
+					}
+
+					// Se valida que el action concuerde con el esperado...
+					if (response.action != ACTION) {
+						LambdaLogger.Log(
+							$"[POST] - [Mensaje] - [IngresarAnonimo] - [{stopwatch.ElapsedMilliseconds} ms] - [{StatusCodes.Status400BadRequest}] - " +
+							$"El token de reCAPTCHA no es válido dado que el Action no es correcto - Action: {response.action} - Expected Action: {ACTION}.");
+						return Results.BadRequest($"El token de reCAPTCHA no es válido dado que el Action no es correcto - Action: {response.action} - Expected Action: {ACTION}.");
+					}
+
+					// Y se valida que el score sea superior al mínimo aceptable...
+					if (response.score <= MIN_SCORE) {
+						LambdaLogger.Log(
+							$"[POST] - [Mensaje] - [IngresarAnonimo] - [{stopwatch.ElapsedMilliseconds} ms] - [{StatusCodes.Status400BadRequest}] - " +
+							$"El token de reCAPTCHA no es válido dado que el Score es inferior al mínimo aceptado - Score: {response.score} - Score Mínimo: {MIN_SCORE}.");
+						return Results.BadRequest($"El token de reCAPTCHA no es válido dado que el Score es inferior al mínimo aceptado - Score: {response.score} - Score Mínimo: {MIN_SCORE}.");
+					}
+
+					entrada.Nombre = entrada.Nombre.Trim();
+					entrada.Correo = entrada.Correo.Trim();
+					entrada.Contenido = entrada.Contenido.Trim();
+
+					// Se valida que el correo sea un correo válido...
+					EmailAddressAttribute emailValidator = new();
+					if (!emailValidator.IsValid(entrada.Correo)) {
+						LambdaLogger.Log(
+							$"[POST] - [Mensaje] - [IngresarAnonimo] - [{stopwatch.ElapsedMilliseconds} ms] - [{StatusCodes.Status400BadRequest}] - " +
+							$"El correo ingresado no es válido.");
+						return Results.BadRequest($"El correo ingresado no es válido.");
+					}
+
+					long id = await mensajeDao.Insertar(new Mensaje {
+						Id = 0,
+						Sub = null,
+						Nombre = entrada.Nombre,
+						Correo = entrada.Correo,
+						Contenido = entrada.Contenido,
+						FechaCreacion = DateTime.UtcNow
+					});
+
+					LambdaLogger.Log(
+						$"[POST] - [Mensaje] - [IngresarAnonimo] - [{stopwatch.ElapsedMilliseconds} ms] - [{StatusCodes.Status200OK}] - " +
+						$"Se ingresa exitosamente el mensaje ID: {id}.");
+
+					return Results.Ok();
+				} catch (Exception ex) {
+					LambdaLogger.Log(
+						$"[POST] - [Mensaje] - [IngresarAnonimo] - [{stopwatch.ElapsedMilliseconds} ms] - [{StatusCodes.Status500InternalServerError}] - " +
 						$"Ocurrió un error al ingresar el mensaje. " +
 						$"{ex}");
 					return Results.Problem($"Ocurrió un error al procesar su solicitud. {(!environment.IsProduction() ? ex : "")}");
