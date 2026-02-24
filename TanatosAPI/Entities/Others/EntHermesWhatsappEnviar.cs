0b8e753c61f35ad6937d0c61e40594d217a5c58e
Benjamín <benjamin.diaze@outlook.com>
2026-02-24 15:11:59 -0300

    Se deja valor por defecto en Lenguaje para Hermes Whatsapp.

--- TanatosAPI/Entities/Others/EntHermesWhatsappEnviar.cs
+++ TanatosAPI/Entities/Others/EntHermesWhatsappEnviar.cs
@@ -3,7 +3,7 @@
 		public required string De { get; set; }
 		public required string Para { get; set; }
 		public required string NombreTemplate { get; set; }
-		public string? Lenguaje { get; set; }
+		public string? Lenguaje { get; set; } = "es_CL";
 		public string[]? ParametrosTitulo {  get; set; }
 		public string[]? ParametrosCuerpo { get; set; }
 		public string[]? ParametrosBoton { get; set; }
