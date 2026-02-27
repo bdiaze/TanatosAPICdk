89107db5d70ce180ec2155dd5378322d1bfacfb0
Benjamín <benjamin.diaze@outlook.com>
2026-02-27 19:50:43 -0300

    Se quita appName en Enviar de Hermes

--- TanatosAPI/Helpers/HermesHelper.cs
+++ TanatosAPI/Helpers/HermesHelper.cs
@@ -6,7 +6,6 @@ using TanatosAPI.Entities.Others;
 
 namespace TanatosAPI.Helpers {
 	public class HermesHelper(VariableEntornoHelper variableEntorno, ApiKeyHelper apiKey) {
-		private readonly string _appName = variableEntorno.Obtener("APP_NAME");
 		private readonly string _hermesBaseUrl = variableEntorno.Obtener("HERMES_API_URL");
 		private readonly string _hermesApiKey = apiKey.ObtenerApiKey(variableEntorno.Obtener("HERMES_API_KEY_ID")).Result;
 
@@ -14,7 +13,7 @@ namespace TanatosAPI.Helpers {
 			using HttpClient client = new();
 			client.DefaultRequestHeaders.Add("x-api-key", _hermesApiKey);
 
-			HttpResponseMessage response = await client.PostAsync(_hermesBaseUrl + $"Correo/Enviar/{Uri.EscapeDataString(_appName)}", new StringContent(JsonSerializer.Serialize(correo, AppJsonSerializerContext.Default.EntHermesCorreoEnviar), Encoding.UTF8, "application/json"));
+			HttpResponseMessage response = await client.PostAsync(_hermesBaseUrl + $"Correo/Enviar", new StringContent(JsonSerializer.Serialize(correo, AppJsonSerializerContext.Default.EntHermesCorreoEnviar), Encoding.UTF8, "application/json"));
 			if (response.StatusCode != HttpStatusCode.OK) {
 				throw new Exception($"Ocurrió un error al enviar el correo. StatusCode: {response.StatusCode} - Content: {await response.Content.ReadAsStringAsync()}");
 			}
@@ -29,7 +28,7 @@ namespace TanatosAPI.Helpers {
 			using HttpClient client = new();
 			client.DefaultRequestHeaders.Add("x-api-key", _hermesApiKey);
 
-			HttpResponseMessage response = await client.PostAsync(_hermesBaseUrl + $"Whatsapp/Enviar/{Uri.EscapeDataString(_appName)}", new StringContent(JsonSerializer.Serialize(whatsapp, AppJsonSerializerContext.Default.EntHermesWhatsappEnviar), Encoding.UTF8, "application/json"));
+			HttpResponseMessage response = await client.PostAsync(_hermesBaseUrl + $"Whatsapp/Enviar", new StringContent(JsonSerializer.Serialize(whatsapp, AppJsonSerializerContext.Default.EntHermesWhatsappEnviar), Encoding.UTF8, "application/json"));
 			if (response.StatusCode != HttpStatusCode.OK) {
 				throw new Exception($"Ocurrió un error al enviar el whatsapp. StatusCode: {response.StatusCode} - Content: {await response.Content.ReadAsStringAsync()}");
 			}
