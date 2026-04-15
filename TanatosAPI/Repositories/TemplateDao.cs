a2f2c2e4c104e5b617b43399e09e7911636a01c3
Benjamín <benjamin.diaze@outlook.com>
2026-04-15 19:40:04 -0400

    Se añade campo RequierePlanEmpresa en dao de Template.

--- TanatosAPI/Repositories/TemplateDao.cs
+++ TanatosAPI/Repositories/TemplateDao.cs
@@ -9,7 +9,7 @@ namespace TanatosAPI.Repositories {
 		public async Task<Template?> ObtenerPorId(long idTemplate) {
 			await using NpgsqlConnection connection = await connectionHelper.ObtenerConexion();
 			return await connection.QueryFirstOrDefaultAsync<Template>(
-				"SELECT ID, ID_TEMPLATE_PADRE, NOMBRE, DESCRIPCION, VIGENCIA FROM TANATOS.TEMPLATE WHERE ID = @IDTEMPLATE",
+				"SELECT ID, ID_TEMPLATE_PADRE, NOMBRE, DESCRIPCION, REQUIERE_PLAN_EMPRESA, VIGENCIA FROM TANATOS.TEMPLATE WHERE ID = @IDTEMPLATE",
 				new { idTemplate }
 			);
 		}
@@ -17,18 +17,20 @@ namespace TanatosAPI.Repositories {
 		public async Task<List<Template>> ObtenerPorVigencia(bool? vigencia) {
 			await using NpgsqlConnection connection = await connectionHelper.ObtenerConexion();
 			return [.. await connection.QueryAsync<Template>(
-				"SELECT ID, ID_TEMPLATE_PADRE, NOMBRE, DESCRIPCION, VIGENCIA FROM TANATOS.TEMPLATE WHERE (VIGENCIA = @VIGENCIA OR @VIGENCIA IS NULL)",
+				"SELECT ID, ID_TEMPLATE_PADRE, NOMBRE, DESCRIPCION, REQUIERE_PLAN_EMPRESA, VIGENCIA FROM TANATOS.TEMPLATE WHERE (VIGENCIA = @VIGENCIA OR @VIGENCIA IS NULL)",
 				new { vigencia }
 			)];
 		}
 
 		public async Task Insertar(Template item, NpgsqlTransaction? transaction = null) {
-			string query = "INSERT INTO TANATOS.TEMPLATE(ID, ID_TEMPLATE_PADRE, NOMBRE, DESCRIPCION, VIGENCIA) VALUES (@ID, @IDTEMPLATEPADRE, @NOMBRE, @DESCRIPCION, @VIGENCIA)";
+			string query = "INSERT INTO TANATOS.TEMPLATE(ID, ID_TEMPLATE_PADRE, NOMBRE, DESCRIPCION, REQUIERE_PLAN_EMPRESA, VIGENCIA) " +
+				"VALUES (@ID, @IDTEMPLATEPADRE, @NOMBRE, @DESCRIPCION, @REQUIEREPLANEMPRESA, @VIGENCIA)";
 			DynamicParameters param = new();
 			param.Add("ID", item.Id);
 			param.Add("IDTEMPLATEPADRE", item.IdTemplatePadre);
 			param.Add("NOMBRE", item.Nombre);
 			param.Add("DESCRIPCION", item.Descripcion);
+			param.Add("REQUIEREPLANEMPRESA", item.RequierePlanEmpresa);
 			param.Add("VIGENCIA", item.Vigencia);
 
 			if (transaction?.Connection != null) {
@@ -40,11 +42,13 @@ namespace TanatosAPI.Repositories {
 		}
 
 		public async Task Actualizar(Template item, NpgsqlTransaction? transaction = null) {
-			string query = "UPDATE TANATOS.TEMPLATE SET ID_TEMPLATE_PADRE = @IDTEMPLATEPADRE, NOMBRE = @NOMBRE, DESCRIPCION = @DESCRIPCION, VIGENCIA = @VIGENCIA WHERE ID = @ID";
+			string query = "UPDATE TANATOS.TEMPLATE SET ID_TEMPLATE_PADRE = @IDTEMPLATEPADRE, NOMBRE = @NOMBRE, DESCRIPCION = @DESCRIPCION, " +
+				"REQUIERE_PLAN_EMPRESA = @REQUIEREPLANEMPRESA, VIGENCIA = @VIGENCIA WHERE ID = @ID";
 			DynamicParameters param = new();
 			param.Add("IDTEMPLATEPADRE", item.IdTemplatePadre);
 			param.Add("NOMBRE", item.Nombre);
 			param.Add("DESCRIPCION", item.Descripcion);
+			param.Add("REQUIEREPLANEMPRESA", item.RequierePlanEmpresa);
 			param.Add("VIGENCIA", item.Vigencia);
 			param.Add("ID", item.Id);
 
