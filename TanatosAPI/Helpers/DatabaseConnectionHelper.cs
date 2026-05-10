798275e2fa9200406519be78b11f71d9913e1fd9
Benjamín <benjamin.diaze@outlook.com>
2026-05-10 12:55:13 -0400

    Se corrige concatenación del MaxPoolSize con connection stirng.

--- TanatosAPI/Helpers/DatabaseConnectionHelper.cs
+++ TanatosAPI/Helpers/DatabaseConnectionHelper.cs
@@ -8,10 +8,13 @@ namespace TanatosAPI.Helpers {
         public async Task<NpgsqlConnection> ObtenerConexion() {
             if (dataSource == null) {
                 string connString = await connectionString.Obtener();
-                connString += "Maximum Pool Size=5;";
 
-                NpgsqlDataSourceBuilder builder = new(connString);
-                dataSource = builder.Build();
+				NpgsqlConnectionStringBuilder stringBuilder = new(connString) {
+					MaxPoolSize = 5
+				};
+
+				NpgsqlDataSourceBuilder dataSourceBuilder = new(stringBuilder.ToString());
+                dataSource = dataSourceBuilder.Build();
             }
 
             return await dataSource.OpenConnectionAsync();
