c4c4d7672d2297944c51976f29674efd15a4d91d
Benjamín <benjamin.diaze@outlook.com>
2026-05-21 20:42:40 -0400

    Se añade bcp para Negocio.

---
+++ TanatosAPI/Business/NegocioBcp.cs
@@ -0,0 +1,20 @@
+﻿using Npgsql;
+using TanatosAPI.Entities.Models;
+using TanatosAPI.Repositories;
+
+namespace TanatosAPI.Business {
+	public class NegocioBcp(NormaSuscritaBcp normaSuscritaBcp, NegocioDao negocioDao, NormaSuscritaDao normaSuscritaDao) {
+		public async Task EliminarNegocio(Negocio negocio, NpgsqlTransaction? transaction = null) {
+			if (negocio.Vigencia) {
+				negocio.FechaEliminacion = DateTime.UtcNow;
+				negocio.Vigencia = false;
+				await negocioDao.Actualizar(negocio, transaction);
+
+				List<NormaSuscrita> normasSuscritas = await normaSuscritaDao.ObtenerPorSub(negocio.Sub, negocio.Id, true, transaction);
+				foreach (NormaSuscrita normaSuscrita in normasSuscritas) {
+					await normaSuscritaBcp.EliminarNormaSuscrita(normaSuscrita, transaction);
+				}
+			}
+		}
+	}
+}
