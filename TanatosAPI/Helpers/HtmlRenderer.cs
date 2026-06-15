9217be5cb7886fc39d57795c240358d6b7c2a0d7
Benjamín <benjamin.diaze@outlook.com>
2026-06-14 20:00:45 -0400

    Se añade lógica para usar template base en el renderizado de html, además se modifican los templates existentes.

--- TanatosAPI/Helpers/HtmlRenderer.cs
+++ TanatosAPI/Helpers/HtmlRenderer.cs
@@ -1,24 +1,43 @@
 ﻿using Scriban;
 using Scriban.Runtime;
+using Scriban.Syntax;
 
 namespace TanatosAPI.Helpers {
     public class HtmlRenderer(IHostEnvironment environment) {
-        public async Task<string> GenerarHtml(string nombreTemplate, ScriptObject? parametros = null) {
-            string path;
-            if (!environment.IsDevelopment()) {
-                path = Path.Combine(AppContext.BaseDirectory, "TemplatesCorreos", nombreTemplate);
-            } else {
-                path = Path.Combine(Directory.GetCurrentDirectory(), "TemplatesCorreos", nombreTemplate);
-            }
+        private async Task<string> ObtenerTemplate(string nombreTemplate) {
+			string path;
+			if (!environment.IsDevelopment()) {
+				path = Path.Combine(AppContext.BaseDirectory, "TemplatesCorreos", nombreTemplate);
+			} else {
+				path = Path.Combine(Directory.GetCurrentDirectory(), "TemplatesCorreos", nombreTemplate);
+			}
+            return await File.ReadAllTextAsync(path);
+		}
 
-            string strTemplate = await File.ReadAllTextAsync(path);
+        public async Task<string> GenerarHtml(string nombreTemplate, ScriptObject? parametros = null, bool conTemplateBase = true) {
+            string strTemplate = await ObtenerTemplate(nombreTemplate);
             Template template = Template.Parse(strTemplate);
             TemplateContext context = new();
             if (parametros != null) {
                 context.PushGlobal(parametros);
             }
 
-            return await template.RenderAsync(context);
+            string contenido = await template.RenderAsync(context);
+
+            if (conTemplateBase) {
+                string strTemplateBase = await ObtenerTemplate("TemplateBase.html");
+				
+				Template templateBase = Template.Parse(strTemplateBase);
+				ScriptObject objectTemplateBase = new() {
+					["CONTENIDO"] = contenido
+				};
+                TemplateContext contextBase = new();
+				contextBase.PushGlobal(objectTemplateBase);
+                return await templateBase.RenderAsync(contextBase);
+			}
+
+
+			return contenido;
         }
     }
 }
