6d3fd9a92a7bb8fc30f3e16ef17a0614e7ed77ca
Benjamín <benjamin.diaze@outlook.com>
2026-06-20 20:37:11 -0400

    Se crea función para calcular vencimiento futuro según fecha de referencia y tipo de periodicidad.

--- TanatosAPI/Business/HistorialNormaSuscritaBcp.cs
+++ TanatosAPI/Business/HistorialNormaSuscritaBcp.cs
@@ -105,5 +105,71 @@ namespace TanatosAPI.Business {
 				}
 			}
 		}
+
+		public DateTime CalcularVencimientoFuturo(DateTime fechaReferenciaUTC, TipoPeriodicidad tipoPeriodicidad) {
+			// Nos aseguramos de que la fecha esté en UTC...
+			if (fechaReferenciaUTC.Kind != DateTimeKind.Utc)
+				throw new InvalidOperationException("La fecha de referencia debe ser UTC");
+
+			// Si la fecha de refencia ya es futura, se devuelve esa misma...
+			DateTime nowUtc = dateTimeProvider.UtcNow;
+			if (fechaReferenciaUTC > nowUtc) return fechaReferenciaUTC;
+
+			// Si el tipo periodicidad no tiene deltas o tienes múltiples deltas, se lanza excepción...
+			int cantDeltas =
+				(tipoPeriodicidad.DeltaAnnos != null ? 1 : 0) +
+				(tipoPeriodicidad.DeltaMeses != null ? 1 : 0) +
+				(tipoPeriodicidad.DeltaDias != null ? 1 : 0);
+
+			if (cantDeltas == 0) 
+				throw new InvalidOperationException($"No se puede calcular vencimiento futuro para este tipo de periodicidad - ID Tipo Periodicidad: {tipoPeriodicidad.Id}");
+
+			if (cantDeltas > 1)
+				throw new InvalidOperationException($"El tipo de periodicidad tiene múltiples deltas definidos - ID Tipo Periodicidad: {tipoPeriodicidad.Id}");
+
+			if (tipoPeriodicidad.DeltaAnnos != null && tipoPeriodicidad.DeltaAnnos <= 0)
+				throw new InvalidOperationException($"El tipo de periodicidad tiene un delta de años inválido - ID Tipo Periodicidad: {tipoPeriodicidad.Id}");
+
+			if (tipoPeriodicidad.DeltaMeses != null && tipoPeriodicidad.DeltaMeses <= 0)
+				throw new InvalidOperationException($"El tipo de periodicidad tiene un delta de meses inválido - ID Tipo Periodicidad: {tipoPeriodicidad.Id}");
+
+			if (tipoPeriodicidad.DeltaDias != null && tipoPeriodicidad.DeltaDias <= 0)
+				throw new InvalidOperationException($"El tipo de periodicidad tiene un delta de dias inválido - ID Tipo Periodicidad: {tipoPeriodicidad.Id}");
+
+			TimeZoneInfo timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("America/Santiago");
+
+			DateTime fechaReferenciaChile = TimeZoneInfo.ConvertTimeFromUtc(fechaReferenciaUTC, timeZoneInfo);
+			DateTime fechaActualChile = TimeZoneInfo.ConvertTimeFromUtc(nowUtc, timeZoneInfo);
+
+			if (tipoPeriodicidad.DeltaAnnos != null) {
+				int fraccion = (int)Math.Floor((double)(fechaActualChile.Year - fechaReferenciaChile.Year) / tipoPeriodicidad.DeltaAnnos.Value);
+				fechaReferenciaChile = fechaReferenciaChile.AddYears(tipoPeriodicidad.DeltaAnnos.Value * fraccion);
+			}
+
+			if (tipoPeriodicidad.DeltaMeses != null) {
+				int diffMeses = ((fechaActualChile.Year - fechaReferenciaChile.Year) * 12) + (fechaActualChile.Month - fechaReferenciaChile.Month);
+				int fraccion = (int)Math.Floor((double)diffMeses / tipoPeriodicidad.DeltaMeses.Value);
+				fechaReferenciaChile = fechaReferenciaChile.AddMonths(tipoPeriodicidad.DeltaMeses.Value * fraccion);
+			}
+
+			if (tipoPeriodicidad.DeltaDias != null) {
+				int fraccion = (int)Math.Floor((fechaActualChile - fechaReferenciaChile).TotalDays / tipoPeriodicidad.DeltaDias.Value);
+				fechaReferenciaChile = fechaReferenciaChile.AddDays(tipoPeriodicidad.DeltaDias.Value * fraccion);
+			}
+			
+			while (fechaReferenciaChile <= fechaActualChile) {
+				if (tipoPeriodicidad.DeltaAnnos != null) {
+					fechaReferenciaChile = fechaReferenciaChile.AddYears(tipoPeriodicidad.DeltaAnnos.Value);
+				}
+				if (tipoPeriodicidad.DeltaMeses != null) {
+					fechaReferenciaChile = fechaReferenciaChile.AddMonths(tipoPeriodicidad.DeltaMeses.Value);
+				}
+				if (tipoPeriodicidad.DeltaDias != null) {
+					fechaReferenciaChile = fechaReferenciaChile.AddDays(tipoPeriodicidad.DeltaDias.Value);
+				}
+			}
+
+			return TimeZoneInfo.ConvertTimeToUtc(fechaReferenciaChile, timeZoneInfo);
+		}
 	}
 }
