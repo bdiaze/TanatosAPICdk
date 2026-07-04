2b134d84a41fde70d6f31c342fb29c83dc4d3ef8
Benjamín <benjamin.diaze@outlook.com>
2026-07-03 23:44:13 -0400

    Se añade ObtenerPorId a bcp de norma suscrita, y se quita eliminarnormasuscrita.

--- TanatosAPI/Business/NormaSuscritaBcp.cs
+++ TanatosAPI/Business/NormaSuscritaBcp.cs
@@ -1,40 +1,21 @@
 ﻿using Npgsql;
 using TanatosAPI.Entities.Models;
-using TanatosAPI.Interfaces;
+using TanatosAPI.Interfaces.Helpers;
+using TanatosAPI.Interfaces.Repositories;
 using TanatosAPI.Repositories;
 
 namespace TanatosAPI.Business {
-	public class NormaSuscritaBcp(IDateTimeProvider dateTimeProvider, NormaSuscritaDao normaSuscritaDao, ProcesoNotificacionBcp procesoNotificacionBcp, FiscalizadorNormaSuscritaBcp fiscalizadorNormaSuscritaBcp, NotificacionNormaSuscritaBcp notificacionNormaSuscritaBcp, HistorialNormaSuscritaBcp historialNormaSuscritaBcp) {
-        public async Task<NormaSuscrita?> ObtenerPorId(long idNormaSuscrita) {
+	public class NormaSuscritaBcp(INormaSuscritaDao normaSuscritaDao) {
+		public bool EstaVigente(NormaSuscrita? normaSuscrita) {
+			return normaSuscrita != null && normaSuscrita.Vigencia;
+		}
+
+		public bool Pertenece(NormaSuscrita normaSuscrita, string sub) {
+			return normaSuscrita.Sub == sub;
+		}
+
+		public async Task<NormaSuscrita?> ObtenerPorId(long idNormaSuscrita) {
             return await normaSuscritaDao.ObtenerPorId(idNormaSuscrita);
         }
-
-        public bool EstaVigente(NormaSuscrita? normaSuscrita) {
-            return normaSuscrita != null && normaSuscrita.Vigencia;
-        }
-
-        public bool Pertenece(NormaSuscrita normaSuscrita, string sub) {
-            return normaSuscrita.Sub == sub;
-        }
-
-        public async Task EliminarNormaSuscrita(NormaSuscrita normaSuscrita, NpgsqlTransaction? transaction = null) {
-			if (normaSuscrita.Vigencia) {
-				DateTime utcNow = dateTimeProvider.UtcNow;
-				if (normaSuscrita.Activado) {
-					normaSuscrita.FechaDesactivacion = utcNow;
-					normaSuscrita.Activado = false;
-				}
-
-				normaSuscrita.FechaEliminacion = utcNow;
-				normaSuscrita.Vigencia = false;
-				await normaSuscritaDao.Actualizar(normaSuscrita, transaction);
-				await procesoNotificacionBcp.ActualizarProgramacionProcesosNormaSuscrita(normaSuscrita.Id, transaction);
-
-
-				await fiscalizadorNormaSuscritaBcp.EliminarPorNormaSuscrita(normaSuscrita, transaction);
-				await notificacionNormaSuscritaBcp.EliminarPorNormaSuscrita(normaSuscrita, transaction);
-				await historialNormaSuscritaBcp.EliminarPorNormaSuscrita(normaSuscrita, false, transaction);
-			}
-		}
 	}
 }
