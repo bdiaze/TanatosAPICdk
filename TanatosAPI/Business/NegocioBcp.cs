1ad24282f1bc24ffbac6b822eb0364fdd534763e
Benjamín <benjamin.diaze@outlook.com>
2026-07-03 23:43:36 -0400

    Se añade ObtenerVigentesPorSub en bcp de negocio, además se quita eliminarnegocio.

--- TanatosAPI/Business/NegocioBcp.cs
+++ TanatosAPI/Business/NegocioBcp.cs
@@ -1,11 +1,13 @@
 ﻿using Npgsql;
 using TanatosAPI.Entities.Models;
 using TanatosAPI.Exceptions;
-using TanatosAPI.Interfaces;
+using TanatosAPI.Interfaces.Business;
+using TanatosAPI.Interfaces.Helpers;
+using TanatosAPI.Interfaces.Repositories;
 using TanatosAPI.Repositories;
 
 namespace TanatosAPI.Business {
-	public class NegocioBcp(IDateTimeProvider dateTimeProvider, NormaSuscritaBcp normaSuscritaBcp, NegocioDao negocioDao, NormaSuscritaDao normaSuscritaDao) : INegocioBcp {
+	public class NegocioBcp(INegocioDao negocioDao) : INegocioBcp {
 		public bool EstaVigente(Negocio? negocio) {
 			return negocio != null && negocio.Vigencia;
 		}
@@ -33,18 +35,9 @@ namespace TanatosAPI.Business {
 		public async Task<Negocio?> ObtenerVigentePorSubYNegocio(string sub, long idNegocio, NpgsqlTransaction? transaction = null) {
 			return (await negocioDao.ObtenerPorSub(sub, true, transaction)).FirstOrDefault(n => n.Id == idNegocio);
         }
-		
-		public async Task EliminarNegocio(Negocio negocio, NpgsqlTransaction? transaction = null) {
-			if (negocio.Vigencia) {
-				negocio.FechaEliminacion = dateTimeProvider.UtcNow;
-				negocio.Vigencia = false;
-				await negocioDao.Actualizar(negocio, transaction);
 
-				List<NormaSuscrita> normasSuscritas = await normaSuscritaDao.ObtenerPorSub(negocio.Sub, negocio.Id, true, transaction);
-				foreach (NormaSuscrita normaSuscrita in normasSuscritas) {
-					await normaSuscritaBcp.EliminarNormaSuscrita(normaSuscrita, transaction);
-				}
-			}
+		public async Task<List<Negocio>>ObtenerVigentesPorSub(string sub, NpgsqlTransaction? transaction = null) {
+			return await negocioDao.ObtenerPorSub(sub, true, transaction);
 		}
 	}
 }
