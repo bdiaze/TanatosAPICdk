c050783f5b534b6fffefc71ce682f43a921bf9eb
Benjamín <benjamin.diaze@outlook.com>
2026-07-03 23:42:56 -0400

    Se añade ObtenerPorFlowCustomerId, RegistrarUsuarioEnFlow y RegistrarTarjetaEnFlow a bcp de usuario.

--- TanatosAPI/Business/UsuarioBcp.cs
+++ TanatosAPI/Business/UsuarioBcp.cs
@@ -1,11 +1,19 @@
-﻿using Npgsql;
+﻿using Microsoft.IdentityModel.Logging;
+using Npgsql;
 using TanatosAPI.Entities.Models;
+using TanatosAPI.Entities.Others.Flow;
 using TanatosAPI.Helpers;
-using TanatosAPI.Interfaces;
+using TanatosAPI.Interfaces.Business;
+using TanatosAPI.Interfaces.Helpers;
+using TanatosAPI.Interfaces.Repositories;
 using TanatosAPI.Repositories;
 
 namespace TanatosAPI.Business {
-	public class UsuarioBcp(ICognitoHelper cognitoHelper, UsuarioDao usuarioDao) {
+	public class UsuarioBcp(IUsuarioDao usuarioDao, ICognitoHelper cognitoHelper, IFlowHelper flowHelper) : IUsuarioBcp {
+		public async Task<Usuario?> ObtenerPorFlowCustomerId(string flowCustomerId, NpgsqlTransaction? transaction = null) {
+			return await usuarioDao.ObtenerPorFlowCustomerId(flowCustomerId, transaction);
+		}	
+
 		public async Task<Usuario> ObtenerInformacionUsuario(string sub, NpgsqlTransaction? transaction = null) {
 			Usuario? usuario = await usuarioDao.Obtener(sub, transaction);
 			if (usuario == null) {
@@ -29,6 +37,28 @@ namespace TanatosAPI.Business {
 			}
 
 			return usuario;
+		}
+
+		public async Task<string> RegistrarUsuarioEnFlow(string sub, NpgsqlTransaction? transaction = null) {
+			Usuario usuario = await ObtenerInformacionUsuario(sub, transaction);
+
+			string nombre = usuario.Nombre ?? "";
+			string apellido = usuario.Apellido ?? "";
+			string correo = usuario.CorreoElectronico ?? throw new InvalidOperationException("No se encuentra registro del correo electrónico del usuario.");
+
+			// Se crea el usuario en flow si no existe...
+			if (usuario.FlowCustomerId == null) {
+				SalFlowCustomerCreate salFlowCustomerCreate = await flowHelper.CustomerCreate($"{nombre} {apellido}".Trim(), correo, sub);
+				usuario.FlowCustomerId = salFlowCustomerCreate.CustomerId;
+				await usuarioDao.Actualizar(usuario, transaction);
+			}
+
+			return usuario.FlowCustomerId!;
+		}
+
+		public async Task<string> RegistrarTarjetaEnFlow(string flowCustomerId) {
+			SalFlowUrlToken salFlowUrlToken = await flowHelper.CustomerRegister(flowCustomerId);
+			return $"{salFlowUrlToken.Url}?token={salFlowUrlToken.Token}";
 		}
 	}
 }
