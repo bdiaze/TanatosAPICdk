13b59a171014873fc10a1a3d6e3f84ab66420e5c
Benjamín <benjamin.diaze@outlook.com>
2026-07-04 01:09:13 -0400

    Se crean unit test para helper de kairos y htmlrenderer.

---
+++ TanatosAPI.Test/Helpers/HtmlRendererTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Logging;
+using NSubstitute;
+using Scriban.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using TanatosAPI.Entities.Others.Flow;
+using TanatosAPI.Helpers;
+using TanatosAPI.Interfaces.Helpers;
+
+namespace TanatosAPI.Test.Helpers {
+	public class HtmlRendererTest {
+		private readonly IHostEnvironment env = Substitute.For<IHostEnvironment>();
+		private readonly IFileHelper fileHelper = Substitute.For<IFileHelper>();
+		private readonly HtmlRenderer htmlRenderer;
+
+		public HtmlRendererTest() {
+			htmlRenderer = new HtmlRenderer(env, fileHelper);
+		}
+
+		[Fact]
+		public async Task GenerarHtml_ValidoSinParametrosSinTemplateBase() {
+			env.EnvironmentName = Environments.Production;
+			fileHelper.ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("nombre-template-test"))).Returns("Template de test");
+
+			string retorno = await htmlRenderer.GenerarHtml("nombre-template-test", null, false);
+			Assert.Equal("Template de test", retorno);
+			await fileHelper.Received(1).ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("nombre-template-test")));
+			await fileHelper.DidNotReceive().ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("TemplateBase.html")));
+		}
+
+		[Fact]
+		public async Task GenerarHtml_ValidoSinParametrosConTemplateBase() {
+			env.EnvironmentName = Environments.Production;
+			fileHelper.ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("nombre-template-test"))).Returns("Template de test");
+			fileHelper.ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("TemplateBase.html"))).Returns("Template {{ CONTENIDO }} base");
+
+			string retorno = await htmlRenderer.GenerarHtml("nombre-template-test");
+			Assert.Equal("Template Template de test base", retorno);
+			await fileHelper.Received(1).ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("nombre-template-test")));
+			await fileHelper.Received(1).ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("TemplateBase.html")));
+		}
+
+		[Fact]
+		public async Task GenerarHtml_ValidoConParametrosConTemplateBase() {
+			env.EnvironmentName = Environments.Production;
+			fileHelper.ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("nombre-template-test"))).Returns("Template {{ PARAMETRO }} test");
+			fileHelper.ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("TemplateBase.html"))).Returns("Template {{ CONTENIDO }} base");
+
+			string retorno = await htmlRenderer.GenerarHtml("nombre-template-test", new ScriptObject() {
+				["PARAMETRO"] = "valor-parametro",
+			});
+			Assert.Equal("Template Template valor-parametro test base", retorno);
+			await fileHelper.Received(1).ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("nombre-template-test")));
+			await fileHelper.Received(1).ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("TemplateBase.html")));
+		}
+
+		[Fact]
+		public async Task GenerarHtml_ValidoConParametrosConTemplateBaseDevelopment() {
+			env.EnvironmentName = Environments.Development;
+			fileHelper.ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("nombre-template-test"))).Returns("Template {{ PARAMETRO }} test");
+			fileHelper.ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("TemplateBase.html"))).Returns("Template {{ CONTENIDO }} base");
+
+			string retorno = await htmlRenderer.GenerarHtml("nombre-template-test", new ScriptObject() {
+				["PARAMETRO"] = "valor-parametro",
+			});
+			Assert.Equal("Template Template valor-parametro test base", retorno);
+			await fileHelper.Received(1).ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("nombre-template-test")));
+			await fileHelper.Received(1).ReadAllTextAsync(Arg.Is<string>(s => s.EndsWith("TemplateBase.html")));
+		}
+	}
+}
---
+++ TanatosAPI.Test/Helpers/KairosHelperTest.cs
@@ -0,0 +1,99 @@
+﻿using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using TanatosAPI.Entities.Others.Hermes;
+using TanatosAPI.Entities.Others.Kairos;
+using TanatosAPI.Helpers;
+using TanatosAPI.Interfaces.Helpers;
+
+namespace TanatosAPI.Test.Helpers {
+	public class KairosHelperTest {
+		private readonly IKairosHttpClient httpClient = Substitute.For<IKairosHttpClient>();
+		private readonly KairosHelper kairosHelper;
+
+		private static readonly DateTime FECHA_DUMMY = new(2026, 1, 15, 14, 0, 0, DateTimeKind.Utc);
+
+		public KairosHelperTest() {
+			kairosHelper = new(httpClient);
+		}
+
+		[Fact]
+		public async Task IngresarProceso_Ok() {
+			httpClient.PostAsync("Procesos", Arg.Any<StringContent>()).Returns(new HttpResponseMessage {
+				StatusCode = HttpStatusCode.OK,
+				Content = new StringContent(
+					JsonSerializer.Serialize(new SalKairosIngresarProceso {
+						IdProceso = "id-proceso-test",
+						IdCalendarizacion = "id-calendarizacion-test",
+						Nombre = "nombre-test",
+						ArnRol = "arn-rol-test",
+						ArnProceso = "arn-proceso-test",
+						Parametros = "parametros-test",
+						Habilitado = true,
+						FechaCreacion = FECHA_DUMMY,
+					}),
+					Encoding.UTF8,
+					"application/json"
+				)
+			});
+
+			SalKairosIngresarProceso retorno = await kairosHelper.IngresarProceso(new EntKairosIngresarProceso {
+				Nombre = "nombre-test",
+				Cron = "cron-test",
+				ArnRol = "arn-rol-test",
+				ArnProceso = "arn-proceso-test",
+				Parametros = "parametros-test",
+				Habilitado = true
+			});
+			Assert.Equal("id-proceso-test", retorno.IdProceso);
+			Assert.Equal("id-calendarizacion-test", retorno.IdCalendarizacion);
+			Assert.Equal("nombre-test", retorno.Nombre);
+			Assert.Equal("arn-rol-test", retorno.ArnRol);
+			Assert.Equal("arn-proceso-test", retorno.ArnProceso);
+			Assert.Equal("parametros-test", retorno.Parametros);
+			Assert.True(retorno.Habilitado);
+			Assert.Equal(FECHA_DUMMY, retorno.FechaCreacion);
+			await httpClient.Received(1).PostAsync("Procesos", Arg.Any<StringContent>());
+		}
+
+		[Fact]
+		public async Task IngresarProceso_Error() {
+			httpClient.PostAsync("Procesos", Arg.Any<StringContent>()).Returns(new HttpResponseMessage {
+				StatusCode = HttpStatusCode.BadRequest,
+			});
+
+			await Assert.ThrowsAsync<HttpRequestException>(() => kairosHelper.IngresarProceso(new EntKairosIngresarProceso {
+				Nombre = "nombre-test",
+				Cron = "cron-test",
+				ArnRol = "arn-rol-test",
+				ArnProceso = "arn-proceso-test",
+				Parametros = "parametros-test",
+				Habilitado = true
+			}));
+			await httpClient.Received(1).PostAsync("Procesos", Arg.Any<StringContent>());
+		}
+
+		[Fact]
+		public async Task EliminarProceso_Ok() {
+			httpClient.DeleteAsync(Arg.Is<string>(s => s.StartsWith("Procesos/"))).Returns(new HttpResponseMessage {
+				StatusCode = HttpStatusCode.OK
+			});
+
+			await kairosHelper.EliminarProceso("id-proceso-test");
+			await httpClient.Received(1).DeleteAsync(Arg.Is<string>(s => s.StartsWith("Procesos/")));
+		}
+
+		[Fact]
+		public async Task EliminarProceso_Error() {
+			httpClient.DeleteAsync(Arg.Is<string>(s => s.StartsWith("Procesos/"))).Returns(new HttpResponseMessage {
+				StatusCode = HttpStatusCode.BadRequest
+			});
+
+			await Assert.ThrowsAsync<HttpRequestException>(() => kairosHelper.EliminarProceso("id-proceso-test"));
+			await httpClient.Received(1).DeleteAsync(Arg.Is<string>(s => s.StartsWith("Procesos/")));
+		}
+	}
+}
