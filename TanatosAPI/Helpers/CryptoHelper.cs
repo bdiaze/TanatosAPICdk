a2f9c37e35fb4a5f9da4f7c7dacdf930667d14a1
Benjamín <benjamin.diaze@outlook.com>
2026-07-03 23:12:25 -0400

    Se cambia cryptohelper a static.

--- TanatosAPI/Helpers/CryptoHelper.cs
+++ TanatosAPI/Helpers/CryptoHelper.cs
@@ -4,12 +4,12 @@ using System.Text;
 
 namespace TanatosAPI.Helpers {
 	[ExcludeFromCodeCoverage]
-	public class CryptoHelper {
-		public string GenerarToken(int bytes = 32) {
+	public static class CryptoHelper {
+		public static string GenerarToken(int bytes = 32) {
 			return Convert.ToBase64String(RandomNumberGenerator.GetBytes(bytes));
 		}
 
-		public string HashSHA256(string input) {
+		public static string HashSHA256(string input) {
 			return Convert.ToBase64String(SHA256.HashData(Encoding.UTF8.GetBytes(input)));
 		}
 	}
