a599de5aee34caf689b91f8f016b88999f2758c3
Benjamín <benjamin.diaze@outlook.com>
2026-07-19 16:06:05 -0400

    Se añaden nuevos campos a EntKairosIngresarProceso.

--- TanatosAPI/Entities/Others/Kairos/EntKairosIngresarProceso.cs
+++ TanatosAPI/Entities/Others/Kairos/EntKairosIngresarProceso.cs
@@ -4,7 +4,9 @@ namespace TanatosAPI.Entities.Others.Kairos {
     [ExcludeFromCodeCoverage]
     public class EntKairosIngresarProceso {
 		public required string Nombre { get; set; }
-		public required string Cron { get; set; }
+		public string? Cron { get; set; }
+		public int? FrecuenciaDias { get; set; }
+		public DateTime? InicioEjecucionUtc { get; set; }
 		public required string ArnRol { get; set; }
 		public required string ArnProceso { get; set; }
 		public required string Parametros { get; set; }
