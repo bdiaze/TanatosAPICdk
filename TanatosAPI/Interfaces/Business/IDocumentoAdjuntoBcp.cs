3bbec67ce6a9ab16cbafc1ad77f7edf96009a657
Benjamín <benjamin.diaze@outlook.com>
2026-07-20 16:22:44 -0400

    Se añade interfaces para bcp de historialnormasuscrita y documentoadjunto.

---
+++ TanatosAPI/Interfaces/Business/IDocumentoAdjuntoBcp.cs
@@ -0,0 +1,18 @@
+﻿using Npgsql;
+using TanatosAPI.Entities.Models;
+
+namespace TanatosAPI.Interfaces.Business {
+	public interface IDocumentoAdjuntoBcp {
+		public bool TamannoValido(long tamanno);
+		public bool MimeValido(string mime);
+		public bool EstaVigente(DocumentoAdjunto? documentoAdjunto);
+		public bool PerteneceAVencimiento(DocumentoAdjunto documentoAdjunto, long idHistorialNormaSuscrita);
+		public Task<DocumentoAdjunto?> ObtenerPorId(long idDocumentoAdjunto);
+		public Task<List<DocumentoAdjunto>> ObtenerVigentesPorHistorialNormaSuscrita(long idHistorialNormaSuscrita);
+		public Task<(string preSignedUrl, Dictionary<string, string> fields, DocumentoAdjunto documentoAdjunto)> GenerarUrlSubida(string sub, long idNegocio, long idNormaSuscrita, long idHistorialNormaSuscrita, string nombreArchivo, string mimeArchivo, long tamannoArchivo);
+		public Task ConfirmarSubida(DocumentoAdjunto documentoAdjunto);
+		public Task<string> GenerarUrlBajada(DocumentoAdjunto documentoAdjunto);
+		public Task Eliminar(DocumentoAdjunto documentoAdjunto, NpgsqlTransaction? transaction = null);
+		public Task EliminarPorHistorialNormaSuscrita(long idHistorialNormaSuscrita, NpgsqlTransaction? transaction = null);
+	}
+}
---
+++ TanatosAPI/Interfaces/Business/IHistorialNormaSuscritaBcp.cs
@@ -0,0 +1,17 @@
+﻿using Npgsql;
+using TanatosAPI.Entities.Models;
+
+namespace TanatosAPI.Interfaces.Business {
+	public interface IHistorialNormaSuscritaBcp {
+		public bool EstaVigente(HistorialNormaSuscrita? historialNormaSuscrita);
+		public bool EstaCompletada(HistorialNormaSuscrita historialNormaSuscrita);
+		public bool VigenteOCompletada(HistorialNormaSuscrita? historialNormaSuscrita);
+		public Task<HistorialNormaSuscrita?> ObtenerPorId(long idHistorialNormaSuscrita);
+		public Task<List<HistorialNormaSuscrita>> ObtenerVigentesPorNormaSuscritaNoCompletadas(long idNormaSuscrita, NpgsqlTransaction? transaction = null);
+		public Task<DateTime> ObtenerProximoVencimiento(long idNormaSuscrita, NpgsqlTransaction? transaction = null);
+		public Task<HistorialNormaSuscrita> Crear(long idNormaSuscrita, DateTime fechaVencimiento, NpgsqlTransaction? transaction = null);
+		public Task Eliminar(HistorialNormaSuscrita historialNormaSuscrita, NpgsqlTransaction? transaction = null);
+		public Task Completar(HistorialNormaSuscrita historialNormaSuscrita, NpgsqlTransaction? transaction = null);
+		public DateTime CalcularVencimientoFuturo(DateTime fechaReferenciaUTC, TipoPeriodicidad tipoPeriodicidad);
+	}
+}
