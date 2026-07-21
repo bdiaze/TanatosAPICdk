8fbffd6a64c2122fa2a2c1c5e9cb0c66639cbc15
bdiaze <bdiaze.cl>
2026-07-21 00:13:10 -0400

    Se añade FrecuenciaDias e InicioEjecucionUtc en EntKairosParametrosProceso.

--- TanatosAPI/Entities/Others/Kairos/EntKairosParametrosProceso.cs
+++ TanatosAPI/Entities/Others/Kairos/EntKairosParametrosProceso.cs
@@ -1,11 +1,14 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
 
 namespace TanatosAPI.Entities.Others.Kairos {
     [ExcludeFromCodeCoverage]
     public class EntKairosParametrosProceso {
 		public required long IdNormaSuscrita { get; set; }
-		public required string Cron { get; set; }
-		public long? IdTipoUnidadTiempoAntelacion { get; set; }
+		public string? Cron { get; set; }
+        public int? FrecuenciaDias { get; set; }
+        public DateTime? InicioEjecucionUtc { get; set; }
+        public long? IdTipoUnidadTiempoAntelacion { get; set; }
         public int? CantAntelacion { get; set; }
 		public bool? EsVencimiento { get; set; }
 		public required bool ProgramarSiguienteEjecucion { get; set; }
