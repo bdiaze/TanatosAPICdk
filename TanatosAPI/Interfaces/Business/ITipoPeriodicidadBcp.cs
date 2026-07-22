ab537eeda345b098047a445f63ae9be77b8fa9e5
bdiaze <bdiaze.cl>
2026-07-21 22:39:26 -0400

    Se añade método ValidarDeltas y ObtenerValidando* a bcp de TipoPeriodicidad.

--- TanatosAPI/Interfaces/Business/ITipoPeriodicidadBcp.cs
+++ TanatosAPI/Interfaces/Business/ITipoPeriodicidadBcp.cs
@@ -4,8 +4,10 @@ using TanatosAPI.Entities.Models;
 namespace TanatosAPI.Interfaces.Business {
 	public interface ITipoPeriodicidadBcp {
 		public bool EstaVigente(TipoPeriodicidad? periodicidad);
-		public Task<TipoPeriodicidad?> ObtenerPorId(long id, NpgsqlTransaction? transaction = null);
-		public Task<List<TipoPeriodicidad>> ObtenerVigentes(NpgsqlTransaction? transaction = null);
+		public void ValidarDeltas(TipoPeriodicidad tipoPeriodicidad);
+        public Task<TipoPeriodicidad?> ObtenerPorId(long id, NpgsqlTransaction? transaction = null);
+		public Task<TipoPeriodicidad> ObtenerValidandoVigencia(long? id, NpgsqlTransaction? transaction = null);
+        public Task<List<TipoPeriodicidad>> ObtenerVigentes(NpgsqlTransaction? transaction = null);
 		public Task<List<TipoPeriodicidad>> ObtenerPorVigencia(bool? vigencia, NpgsqlTransaction? transaction = null);
 		public Task<TipoPeriodicidad> Crear(long id, string nombre, string? descripcion, string? cron, int? frecuenciaDias, int? deltaDias, int? deltaMeses, int? deltaAnnos, int orden, bool vigencia, NpgsqlTransaction? transaction = null);
 		public Task<TipoPeriodicidad> Modificar(TipoPeriodicidad tipoPeriodicidad, NpgsqlTransaction? transaction = null);
