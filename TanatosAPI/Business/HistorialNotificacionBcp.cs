f0d86f5d2bbd28137eff47e6ccb66111fba92311
bdiaze <bdiaze.cl>
2026-07-21 22:36:28 -0400

    Se añade ObtenerPorCodigoAccesoValidando* a bcp de HistorialNotificacion.

--- TanatosAPI/Business/HistorialNotificacionBcp.cs
+++ TanatosAPI/Business/HistorialNotificacionBcp.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿using Npgsql;
+using System.ComponentModel;
 using TanatosAPI.Entities.Models;
+using TanatosAPI.Exceptions;
 using TanatosAPI.Helpers;
 using TanatosAPI.Interfaces.Business;
 using TanatosAPI.Interfaces.Helpers;
@@ -16,8 +18,21 @@ namespace TanatosAPI.Business {
             return historialNotificacion.FechaCaducidadCodigoAcceso == null || historialNotificacion.FechaCaducidadCodigoAcceso >= dateTimeProvider.UtcNow;
         }
         
-        public async Task<HistorialNotificacion?> ObtenerPorCodigoAcceso(string codigoAcceso) {
-            return await historialNotificacionDao.ObtenerPorCodigoAcceso(CryptoHelper.HashSHA256(codigoAcceso), null);
+        public async Task<HistorialNotificacion?> ObtenerPorCodigoAcceso(string codigoAcceso, NpgsqlTransaction? transaction = null) {
+            return await historialNotificacionDao.ObtenerPorCodigoAcceso(CryptoHelper.HashSHA256(codigoAcceso), null, transaction);
+        }
+
+        public async Task<HistorialNotificacion> ObtenerPorCodigoAccesoValidandoVigencia(string codigoAcceso, NpgsqlTransaction? transaction = null) {
+            HistorialNotificacion? historialNotificacion = await ObtenerPorCodigoAcceso(codigoAcceso, transaction);
+            if (!EstaVigente(historialNotificacion)) {
+                throw new ErrorValidacion(TipoErrorValidacion.AccesoCaducado, "La notificación no está vigente", "El código de acceso es inválido.");
+            }
+
+            if (!CodigoAccesoVigente(historialNotificacion!)) {
+                throw new ErrorValidacion(TipoErrorValidacion.AccesoCaducado, "El código de acceso ha caducado", "El código de acceso es inválido.");
+            }
+
+            return historialNotificacion!;
         }
     }
 }
--- TanatosAPI/Interfaces/Business/IHistorialNotificacionBcp.cs
+++ TanatosAPI/Interfaces/Business/IHistorialNotificacionBcp.cs
@@ -1,10 +1,12 @@
-﻿using TanatosAPI.Entities.Models;
+﻿using Npgsql;
+using TanatosAPI.Entities.Models;
 
 namespace TanatosAPI.Interfaces.Business {
     public interface IHistorialNotificacionBcp {
         public bool EstaVigente(HistorialNotificacion? historialNotificacion);
         public bool CodigoAccesoVigente(HistorialNotificacion historialNotificacion);
-        public Task<HistorialNotificacion?> ObtenerPorCodigoAcceso(string codigoAcceso);
+        public Task<HistorialNotificacion?> ObtenerPorCodigoAcceso(string codigoAcceso, NpgsqlTransaction? transaction = null);
+        public Task<HistorialNotificacion> ObtenerPorCodigoAccesoValidandoVigencia(string codigoAcceso, NpgsqlTransaction? transaction = null);
 
     }
 }
