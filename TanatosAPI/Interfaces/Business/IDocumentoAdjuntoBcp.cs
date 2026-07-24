5169966d8e734dad3cf95be9dd9d34f3159098d7
Benjamín <benjamin.diaze@outlook.com>
2026-07-23 23:00:01 -0400

    Se actualiza interface de DocumentoAdjuntoBcp.

--- TanatosAPI/Interfaces/Business/IDocumentoAdjuntoBcp.cs
+++ TanatosAPI/Interfaces/Business/IDocumentoAdjuntoBcp.cs
@@ -6,9 +6,12 @@ namespace TanatosAPI.Interfaces.Business {
 		public bool TamannoValido(long tamanno);
 		public bool MimeValido(string mime);
 		public bool EstaVigente(DocumentoAdjunto? documentoAdjunto);
-		public bool PerteneceAVencimiento(DocumentoAdjunto documentoAdjunto, long idHistorialNormaSuscrita);
-		public Task<DocumentoAdjunto?> ObtenerPorId(long idDocumentoAdjunto);
-		public Task<List<DocumentoAdjunto>> ObtenerVigentesPorHistorialNormaSuscrita(long idHistorialNormaSuscrita);
+		public bool FueRecepcionado(DocumentoAdjunto documentoAdjunto);
+		public bool Pertenece(DocumentoAdjunto documentoAdjunto, long idHistorialNormaSuscrita);
+		public List<DocumentoAdjunto> FiltrarVigentes(List<DocumentoAdjunto> documentos);
+		public List<DocumentoAdjunto> FiltrarRecepcionados(List<DocumentoAdjunto> documentos);
+		public Task<DocumentoAdjunto?> Obtener(long idDocumentoAdjunto, NpgsqlTransaction? transaction = null);
+		public Task<List<DocumentoAdjunto>> ObtenerPorVencimiento(long idHistorialNormaSuscrita, bool filtrarVigentes = false, bool filtrarRecepcionados = false, NpgsqlTransaction? transaction = null);
 		public Task<(string preSignedUrl, Dictionary<string, string> fields, DocumentoAdjunto documentoAdjunto)> GenerarUrlSubida(string sub, long idNegocio, long idNormaSuscrita, long idHistorialNormaSuscrita, string nombreArchivo, string mimeArchivo, long tamannoArchivo);
 		public Task ConfirmarSubida(DocumentoAdjunto documentoAdjunto);
 		public Task<string> GenerarUrlBajada(DocumentoAdjunto documentoAdjunto);
