0ab32c463fe5edd8cd5170a8e12e3a28412a749a
bdiaze <bdiaze.cl>
2026-07-29 22:42:56 -0400

    Se añaden métodos EstaVigente, TieneCargo, FiltrarVigentes, FiltrarCargo y ObtenerPorSubYNegocio (con filtro) a bcp de Empleado.

--- TanatosAPI/Interfaces/Business/IEmpleadoBcp.cs
+++ TanatosAPI/Interfaces/Business/IEmpleadoBcp.cs
@@ -1,7 +1,11 @@
 ﻿using Npgsql;
+using TanatosAPI.Entities.Models;
 
 namespace TanatosAPI.Interfaces.Business {
 	public interface IEmpleadoBcp {
-		public Task DesasociarCargo(string sub, long idNegocio, long idCargo, NpgsqlTransaction? transaction = null);
+        public bool EstaVigente(Empleado? empleado);
+        public List<Empleado> FiltrarVigentes(List<Empleado> empleados);
+        public Task<List<Empleado>> ObtenerPorSubYNegocio(string sub, long idNegocio, bool filtrarVigente = false, long? filtrarIdCargo = null, NpgsqlTransaction? transaction = null);
+        public Task DesasociarCargo(string sub, long idNegocio, long idCargo, NpgsqlTransaction? transaction = null);
 	}
 }
