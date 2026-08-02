d58d276bb0b3e8d5a79be2bb2d1951a05fa47dd5
Benjamín <benjamin.diaze@outlook.com>
2026-08-01 23:53:54 -0400

    Se crea test base con dummy de HistorialNormaSuscritaBcp.

--- TanatosAPI.Test/Business/HistorialNormaSuscritaBcpTest.cs
+++ TanatosAPI.Test/Business/HistorialNormaSuscritaBcpTest.cs
@@ -1,8 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TanatosAPI.Entities.Models;
 
 namespace TanatosAPI.Test.Business {
-    internal class HistorialNormaSuscritaBcpTest {
+    public class HistorialNormaSuscritaBcpTest {
+		private static readonly DateTime FECHA_DUMMY = new(2026, 1, 15, 14, 0, 0, DateTimeKind.Utc);
+
+		public static HistorialNormaSuscrita HistorialNormaSuscritaDummy(
+            long id = 1,
+            long idNormaSuscrita = 10,
+            DateTime? fechaVencimiento = null,
+            DateTime? fechaCompletitud = null,
+            DateTime? fechaCreacion = null,
+            DateTime? fechaEliminacion = null,
+            bool vigencia = true
+		) => new() { 
+            Id = id,
+            IdNormaSuscrita = idNormaSuscrita,
+            FechaVencimiento = fechaVencimiento ?? FECHA_DUMMY,
+            FechaCompletitud = fechaCompletitud,
+            FechaCreacion = fechaCreacion ?? FECHA_DUMMY,
+            FechaEliminacion = fechaEliminacion,
+            Vigencia = vigencia
+		};
     }
 }
