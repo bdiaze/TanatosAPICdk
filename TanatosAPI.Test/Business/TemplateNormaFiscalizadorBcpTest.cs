0891c8aff68b0587e801df3d24b882187800bf22
Benjamín <benjamin.diaze@outlook.com>
2026-08-01 23:49:52 -0400

    Se crean test base con dummy para FiscalizadorNormaSuscritaBcp, NotificacionNormaSuscritaBcp, TemplateBcp, TemplateNormaBcp, TemplateNormaFiscalizadorBcp, TemplateNormaNotificacionBcp, TipoFiscalizadorBcp y TipoUnidadTiempoBcp.

---
+++ TanatosAPI.Test/Business/TemplateNormaFiscalizadorBcpTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TanatosAPI.Entities.Models;
+
+namespace TanatosAPI.Test.Business {
+	public class TemplateNormaFiscalizadorBcpTest {
+		public static TemplateNormaFiscalizador TemplateNormaFiscalizadorDummy(
+			long idTemplate = 10,
+			long idNorma = 100,
+			long idTipoFiscalizador = 1000
+		) => new() { 
+			IdTemplate = idTemplate,
+			IdNorma = idNorma,
+			IdTipoFiscalizador = idTipoFiscalizador
+		};
+	}
+}
