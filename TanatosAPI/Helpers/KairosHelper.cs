ec64a4949120f7a64c9301c1b6537a5f31b64249
Benjamín <benjamin.diaze@outlook.com>
2026-08-20 15:21:22 -0400

    Se añaden métodos IngresarVariosProcesos y EliminarVariosProcesos a KairosHelper.

--- TanatosAPI/Helpers/KairosHelper.cs
+++ TanatosAPI/Helpers/KairosHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Text;
 using System.Text.Json;
@@ -9,26 +10,32 @@ namespace TanatosAPI.Helpers {
     public class KairosHelper(IKairosHttpClient httpClient) : IKairosHelper {
 		public async Task<SalKairosIngresarProceso> IngresarProceso(EntKairosIngresarProceso proceso) {
 			HttpResponseMessage response = await httpClient.PostAsync("Procesos", new StringContent(JsonSerializer.Serialize(proceso, AppJsonSerializerContext.Default.EntKairosIngresarProceso), Encoding.UTF8, "application/json"));
-			if (response.StatusCode != HttpStatusCode.OK) {
-				throw new HttpRequestException(
-					$"Ocurrió un error al ingresar el proceso. StatusCode: {response.StatusCode} - Content: {await response.Content.ReadAsStringAsync()}",
-					inner: null,
-					statusCode: response.StatusCode
-				);
-			}
-
+			response.EnsureSuccessStatusCode();
 			return JsonSerializer.Deserialize(await response.Content.ReadAsStringAsync(), AppJsonSerializerContext.Default.SalKairosIngresarProceso)!;
+		}
+
+		public async Task<List<SalKairosIngresarProceso>> IngresarVariosProcesos(List<EntKairosIngresarProceso> procesos) {
+			if (procesos.Count == 0) return []; 
+
+			HttpResponseMessage response = await httpClient.PostAsync("Procesos/Varios", new StringContent(JsonSerializer.Serialize(procesos, AppJsonSerializerContext.Default.ListEntKairosIngresarProceso), Encoding.UTF8, "application/json"));
+			response.EnsureSuccessStatusCode();
+			return JsonSerializer.Deserialize(await response.Content.ReadAsStringAsync(), AppJsonSerializerContext.Default.ListSalKairosIngresarProceso)!;
 		}
 
 		public async Task EliminarProceso(string idProceso) {
 			HttpResponseMessage response = await httpClient.DeleteAsync($"Procesos/{Uri.EscapeDataString(idProceso)}");
-			if (response.StatusCode != HttpStatusCode.OK) {
-				throw new HttpRequestException(
-					$"Ocurrió un error al eliminar el proceso. StatusCode: {response.StatusCode} - Content: {await response.Content.ReadAsStringAsync()}",
-					inner: null,
-					statusCode: response.StatusCode
-				);
-			}
+			response.EnsureSuccessStatusCode();
+		}
+
+		public async Task EliminarVariosProcesos(List<string> idsProcesos) {
+			if (idsProcesos.Count == 0) return;
+
+			HttpRequestMessage request = new(HttpMethod.Delete, "Procesos/Varios") { 
+				Content = JsonContent.Create(idsProcesos, AppJsonSerializerContext.Default.ListString)
+			};
+
+			HttpResponseMessage response = await httpClient.SendAsync(request);
+			response.EnsureSuccessStatusCode();
 		}
 	}
 }
--- TanatosAPI/Interfaces/Helpers/IKairosHelper.cs
+++ TanatosAPI/Interfaces/Helpers/IKairosHelper.cs
@@ -3,6 +3,8 @@
 namespace TanatosAPI.Interfaces.Helpers {
 	public interface IKairosHelper {
 		public Task<SalKairosIngresarProceso> IngresarProceso(EntKairosIngresarProceso proceso);
+		public Task<List<SalKairosIngresarProceso>> IngresarVariosProcesos(List<EntKairosIngresarProceso> procesos);
 		public Task EliminarProceso(string idProceso);
+		public Task EliminarVariosProcesos(List<string> idsProcesos);
 	}
 }
