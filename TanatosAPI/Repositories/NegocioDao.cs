a769754fc4bd4f06e464df409bf1d58ca9ba9891
Benjamín <benjamin.diaze@outlook.com>
2026-09-01 20:58:44 -0400

    Se actualiza dao de negocio para integrar mision, vision y valores.

--- TanatosAPI/Repositories/NegocioDao.cs
+++ TanatosAPI/Repositories/NegocioDao.cs
@@ -12,7 +12,7 @@ namespace TanatosAPI.Repositories {
 
 		public async Task<Negocio?> Obtener(long id, NpgsqlTransaction? transaction = null) {
 			string query =
-				"SELECT ID, SUB, NOMBRE, DIRECCION, ID_TIPO_ACTIVIDAD, FECHA_CREACION, FECHA_ELIMINACION, VIGENCIA FROM TANATOS.NEGOCIO " +
+				"SELECT ID, SUB, NOMBRE, DIRECCION, ID_TIPO_ACTIVIDAD, MISION, VISION, VALORES, FECHA_CREACION, FECHA_ELIMINACION, VIGENCIA FROM TANATOS.NEGOCIO " +
 				"WHERE ID = @ID";
 
 			bool disposeConnection = transaction?.Connection == null;
@@ -27,14 +27,17 @@ namespace TanatosAPI.Repositories {
 				Negocio? retorno = null;
 				if (await reader.ReadAsync()) {
 					retorno = new Negocio {
-						Id = reader.GetInt64(0),
-						Sub = reader.GetString(1),
-						Nombre = reader.GetString(2),
-						Direccion = await reader.IsDBNullAsync(3) ? null : reader.GetString(3),
-						IdTipoActividad = await reader.IsDBNullAsync(4) ? null : reader.GetInt64(4),
-						FechaCreacion = reader.GetDateTime(5),
-						FechaEliminacion = await reader.IsDBNullAsync(6) ? null : reader.GetDateTime(6),
-						Vigencia = reader.GetBoolean(7)
+						Id = reader.GetInt64(reader.GetOrdinal("ID")),
+						Sub = reader.GetString(reader.GetOrdinal("SUB")),
+						Nombre = reader.GetString(reader.GetOrdinal("NOMBRE")),
+						Direccion = await reader.IsDBNullAsync(reader.GetOrdinal("DIRECCION")) ? null : reader.GetString(reader.GetOrdinal("DIRECCION")),
+						IdTipoActividad = await reader.IsDBNullAsync(reader.GetOrdinal("ID_TIPO_ACTIVIDAD")) ? null : reader.GetInt64(reader.GetOrdinal("ID_TIPO_ACTIVIDAD")),
+						Mision = await reader.IsDBNullAsync(reader.GetOrdinal("MISION")) ? null : reader.GetString(reader.GetOrdinal("MISION")),
+						Vision = await reader.IsDBNullAsync(reader.GetOrdinal("VISION")) ? null : reader.GetString(reader.GetOrdinal("VISION")),
+						Valores = await reader.IsDBNullAsync(reader.GetOrdinal("VALORES")) ? null : reader.GetString(reader.GetOrdinal("VALORES")),
+						FechaCreacion = reader.GetDateTime(reader.GetOrdinal("FECHA_CREACION")),
+						FechaEliminacion = await reader.IsDBNullAsync(reader.GetOrdinal("FECHA_ELIMINACION")) ? null : reader.GetDateTime(reader.GetOrdinal("FECHA_ELIMINACION")),
+						Vigencia = reader.GetBoolean(reader.GetOrdinal("VIGENCIA"))
 					};
 				}
 				return retorno;
@@ -47,7 +50,7 @@ namespace TanatosAPI.Repositories {
 
 		public async Task<List<Negocio>> ObtenerPorSub(string sub, bool? vigencia = true, NpgsqlTransaction? transaction = null) {
 			string query =
-				"SELECT ID, SUB, NOMBRE, DIRECCION, ID_TIPO_ACTIVIDAD, FECHA_CREACION, FECHA_ELIMINACION, VIGENCIA FROM TANATOS.NEGOCIO " +
+				"SELECT ID, SUB, NOMBRE, DIRECCION, ID_TIPO_ACTIVIDAD, MISION, VISION, VALORES, FECHA_CREACION, FECHA_ELIMINACION, VIGENCIA FROM TANATOS.NEGOCIO " +
 				"WHERE SUB = @SUB AND (VIGENCIA = @VIGENCIA OR @VIGENCIA IS NULL)";
 
 			bool disposeConnection = transaction?.Connection == null;
@@ -63,14 +66,17 @@ namespace TanatosAPI.Repositories {
 				List<Negocio> retorno = [];
 				while (await reader.ReadAsync()) {
 					retorno.Add(new Negocio {
-						Id = reader.GetInt64(0),
-						Sub = reader.GetString(1),
-						Nombre = reader.GetString(2),
-						Direccion = await reader.IsDBNullAsync(3) ? null : reader.GetString(3),
-						IdTipoActividad = await reader.IsDBNullAsync(4) ? null : reader.GetInt64(4),
-						FechaCreacion = reader.GetDateTime(5),
-						FechaEliminacion = await reader.IsDBNullAsync(6) ? null : reader.GetDateTime(6),
-						Vigencia = reader.GetBoolean(7)
+						Id = reader.GetInt64(reader.GetOrdinal("ID")),
+						Sub = reader.GetString(reader.GetOrdinal("SUB")),
+						Nombre = reader.GetString(reader.GetOrdinal("NOMBRE")),
+						Direccion = await reader.IsDBNullAsync(reader.GetOrdinal("DIRECCION")) ? null : reader.GetString(reader.GetOrdinal("DIRECCION")),
+						IdTipoActividad = await reader.IsDBNullAsync(reader.GetOrdinal("ID_TIPO_ACTIVIDAD")) ? null : reader.GetInt64(reader.GetOrdinal("ID_TIPO_ACTIVIDAD")),
+						Mision = await reader.IsDBNullAsync(reader.GetOrdinal("MISION")) ? null : reader.GetString(reader.GetOrdinal("MISION")),
+						Vision = await reader.IsDBNullAsync(reader.GetOrdinal("VISION")) ? null : reader.GetString(reader.GetOrdinal("VISION")),
+						Valores = await reader.IsDBNullAsync(reader.GetOrdinal("VALORES")) ? null : reader.GetString(reader.GetOrdinal("VALORES")),
+						FechaCreacion = reader.GetDateTime(reader.GetOrdinal("FECHA_CREACION")),
+						FechaEliminacion = await reader.IsDBNullAsync(reader.GetOrdinal("FECHA_ELIMINACION")) ? null : reader.GetDateTime(reader.GetOrdinal("FECHA_ELIMINACION")),
+						Vigencia = reader.GetBoolean(reader.GetOrdinal("VIGENCIA"))
 					});
 				}
 				return retorno;
@@ -83,8 +89,8 @@ namespace TanatosAPI.Repositories {
 
 		public async Task<long> Insertar(Negocio item, NpgsqlTransaction? transaction = null) {
             string query =
-                "INSERT INTO TANATOS.NEGOCIO(SUB, NOMBRE, DIRECCION, ID_TIPO_ACTIVIDAD, FECHA_CREACION, FECHA_ELIMINACION, VIGENCIA) " +
-                "VALUES (@SUB, @NOMBRE, @DIRECCION, @IDTIPOACTIVIDAD, @FECHACREACION, @FECHAELIMINACION, @VIGENCIA) " +
+				"INSERT INTO TANATOS.NEGOCIO(SUB, NOMBRE, DIRECCION, ID_TIPO_ACTIVIDAD, MISION, VISION, VALORES, FECHA_CREACION, FECHA_ELIMINACION, VIGENCIA) " +
+				"VALUES (@SUB, @NOMBRE, @DIRECCION, @IDTIPOACTIVIDAD, @MISION, @VISION, @VALORES, @FECHACREACION, @FECHAELIMINACION, @VIGENCIA) " +
                 "RETURNING ID";
 
             bool disposeConnection = transaction?.Connection == null;
@@ -96,7 +102,10 @@ namespace TanatosAPI.Repositories {
                 command.Parameters.AddWithValue("NOMBRE", item.Nombre);
                 command.Parameters.AddWithValue("DIRECCION", (object?)item.Direccion ?? DBNull.Value);
                 command.Parameters.AddWithValue("IDTIPOACTIVIDAD", (object?)item.IdTipoActividad ?? DBNull.Value);
-                command.Parameters.AddWithValue("FECHACREACION", item.FechaCreacion);
+				command.Parameters.AddWithValue("MISION", (object?)item.Mision ?? DBNull.Value);
+				command.Parameters.AddWithValue("VISION", (object?)item.Vision ?? DBNull.Value);
+				command.Parameters.AddWithValue("VALORES", (object?)item.Valores ?? DBNull.Value);
+				command.Parameters.AddWithValue("FECHACREACION", item.FechaCreacion);
                 command.Parameters.AddWithValue("FECHAELIMINACION", (object?)item.FechaEliminacion ?? DBNull.Value);
                 command.Parameters.AddWithValue("VIGENCIA", item.Vigencia);
                 return Convert.ToInt64(await command.ExecuteScalarAsync());
@@ -110,7 +119,7 @@ namespace TanatosAPI.Repositories {
 		public async Task Actualizar(Negocio item, NpgsqlTransaction? transaction = null) {
 			string query =
 				"UPDATE TANATOS.NEGOCIO SET SUB = @SUB, NOMBRE = @NOMBRE, DIRECCION = @DIRECCION, " +
-                "ID_TIPO_ACTIVIDAD = @IDTIPOACTIVIDAD, FECHA_CREACION = @FECHACREACION, " +
+				"ID_TIPO_ACTIVIDAD = @IDTIPOACTIVIDAD, MISION = @MISION, VISION = @VISION, VALORES = @VALORES, FECHA_CREACION = @FECHACREACION, " +
                 "FECHA_ELIMINACION = @FECHAELIMINACION, " +
 				"VIGENCIA = @VIGENCIA WHERE ID = @ID";
 
@@ -123,7 +132,10 @@ namespace TanatosAPI.Repositories {
                 command.Parameters.AddWithValue("NOMBRE", item.Nombre);
                 command.Parameters.AddWithValue("DIRECCION", (object?)item.Direccion ?? DBNull.Value);
                 command.Parameters.AddWithValue("IDTIPOACTIVIDAD", (object?)item.IdTipoActividad ?? DBNull.Value);
-                command.Parameters.AddWithValue("FECHACREACION", item.FechaCreacion);
+				command.Parameters.AddWithValue("MISION", (object?)item.Mision ?? DBNull.Value);
+				command.Parameters.AddWithValue("VISION", (object?)item.Vision ?? DBNull.Value);
+				command.Parameters.AddWithValue("VALORES", (object?)item.Valores ?? DBNull.Value);
+				command.Parameters.AddWithValue("FECHACREACION", item.FechaCreacion);
                 command.Parameters.AddWithValue("FECHAELIMINACION", (object?)item.FechaEliminacion ?? DBNull.Value);
                 command.Parameters.AddWithValue("VIGENCIA", item.Vigencia);
                 command.Parameters.AddWithValue("ID", item.Id);
