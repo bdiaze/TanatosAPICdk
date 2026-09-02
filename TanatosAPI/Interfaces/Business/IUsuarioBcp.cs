35eeac7446745e4b58359d4010d0126d67ad9125
Benjamín <benjamin.diaze@outlook.com>
2026-09-02 19:09:20 -0400

    Se crea lógica de configuración inicial, con creación de Usuario con datos desde cognito y con suscripción a planes gratuitos.

--- TanatosAPI/Interfaces/Business/IUsuarioBcp.cs
+++ TanatosAPI/Interfaces/Business/IUsuarioBcp.cs
@@ -4,6 +4,8 @@ using TanatosAPI.Entities.Models;
 namespace TanatosAPI.Interfaces.Business {
 	public interface IUsuarioBcp {
 		public Task<Usuario?> ObtenerPorFlowCustomerId(string flowCustomerId, NpgsqlTransaction? transaction = null);
+		public Task<Usuario> Crear(string sub, string userName, string? flowCustomerId, string? nombre, string? apellido, string? correoElectronico, NpgsqlTransaction? transaction = null);
+		public Task<Usuario> CargarDesdeCognitoSiNoExiste(string userName, NpgsqlTransaction? transaction = null);
 		public Task<Usuario> ObtenerInformacionUsuario(string sub, NpgsqlTransaction? transaction = null);
 		public Task<string> RegistrarUsuarioEnFlow(string sub, NpgsqlTransaction? transaction = null);
 		public Task<string> RegistrarTarjetaEnFlow(string flowCustomerId);
--- TanatosAPI/UseCases/PerfilUseCase.cs
+++ TanatosAPI/UseCases/PerfilUseCase.cs
@@ -1,7 +1,39 @@
-﻿namespace TanatosAPI.UseCases {
-	public class PerfilUseCase {
-		public async Task ConfiguracionInicial(string userName) {
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Transactions;
+using TanatosAPI.Business;
+using TanatosAPI.Entities.Models;
+using TanatosAPI.Interfaces.Business;
+using TanatosAPI.Interfaces.Helpers;
 
+namespace TanatosAPI.UseCases {
+	public class PerfilUseCase(IDatabaseConnectionHelper connectionHelper, IUsuarioBcp usuarioBcp, SuscripcionUseCase suscripcionUseCase) {
+		public async Task ConfiguracionInicial(string userName, IDatabaseTransaction? transaction = null) {
+			bool ownsTransaction = transaction == null;
+			IDatabaseConnection? connection = null;
+
+			try {
+				if (ownsTransaction) {
+					connection = await connectionHelper.ObtenerConexionWrapper();
+					transaction = await connection.BeginTransactionAsync();
+				}
+
+				Usuario usuario = await usuarioBcp.CargarDesdeCognitoSiNoExiste(userName, transaction!.NpgsqlTransaction());
+				_ = await suscripcionUseCase.SuscribirseAPlanesGratuitos(usuario.Sub, transaction);
+
+				if (ownsTransaction) {
+					await transaction!.CommitAsync();
+				}
+			} catch {
+				if (ownsTransaction && transaction != null) {
+					await transaction.RollbackAsync();
+				}
+				throw;
+			} finally {
+				if (ownsTransaction) {
+					if (transaction != null) await transaction.DisposeAsync();
+					if (connection != null) await connection.DisposeAsync();
+				}
+			}
 		}
 	}
 }
